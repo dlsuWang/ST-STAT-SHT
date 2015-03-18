291038fae2b498c9ed8aca7be6b4d7caccb10238
Austin Fernandez <ryanaustinf@yahoo.com>
2015-03-18 10:31:41 +0800

    Changed Look And Feel to Nimbus.

--- Project/src/yahtzee/Main.java
+++ Project/src/yahtzee/Main.java
@@ -1,7 +1,19 @@
 package yahtzee;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Main {
 	public static void main( String[] args ) {
-		
+		try {
+			for( LookAndFeelInfo lafi: UIManager.getInstalledLookAndFeels() ) {
+				if( lafi.getName().equals("Nimbus") ) {
+					UIManager.setLookAndFeel( lafi.getClassName() );
+					break;
+				}
+			}
+		} catch( Exception e ) {
+			e.printStackTrace();
+		}
 	}
 }
