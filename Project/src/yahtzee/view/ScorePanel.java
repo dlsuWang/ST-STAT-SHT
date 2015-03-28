28b575bd60beeff1e3db7eeb0e8d974547dc9a3a
Austin Fernandez <ryanaustinf@yahoo.com>
2015-03-28 11:27:31 +0800

    Updated ScorePanel

--- Project/src/yahtzee/view/ScorePanel.java
+++ Project/src/yahtzee/view/ScorePanel.java
@@ -1,8 +1,10 @@
 package yahtzee.view;
 
+import javax.swing.JPanel;
+
 import yahtzee.controller.IController;
 
-public class ScorePanel implements IView {
+public class ScorePanel extends JPanel implements IView {
 	
 	public void setModel( Object o ) {
 		
